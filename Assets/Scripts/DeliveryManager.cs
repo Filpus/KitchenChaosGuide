16ec4f7a738cc49c85658bbf4fea53065253fb46
Filpus <fil-pus@wp.pl>
2025-02-22 01:09:28 +0100

    Delivery Manager

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,7 +1,75 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DeliveryManager : MonoBehaviour
 {
-    private List<RecipeSO> waitngRecipeSOList;
+    
+    public static DeliveryManager Instance { get; private set; }
+    [SerializeField] private RecipeListSO recipeListSO;
+    private List<RecipeSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+    public void Awake()
+    {
+        waitingRecipeSOList = new List<RecipeSO>();
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        spawnRecipeTimer -= Time.deltaTime;
+        if (spawnRecipeTimer <= 0f)
+        {
+            spawnRecipeTimer = spawnRecipeTimerMax;
+
+            if (waitingRecipeSOList.Count < waitingRecipesMax)
+            {
+                RecipeSO waitingRecipeSO =  recipeListSO.recipeListSoList[Random.Range(0, recipeListSO.recipeListSoList.Count)];
+                Debug.Log(waitingRecipeSO.recipeName);
+                waitingRecipeSOList.Add(waitingRecipeSO);
+            }
+        }
+    }
+
+    public bool DeliverRecipe(PlateKitchenObject plateKitchenObject)
+    {
+        for (int i = 0; i < waitingRecipeSOList.Count; i++)
+        {
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+
+            if (waitingRecipeSO.kitchenObjectSOList.Count == plateKitchenObject.getKitchenObjectSOList().Count)
+            {
+                bool plateContentsMatchesRecipe = true;
+                foreach (KitchenObjectSO recipeKitchenObjectSo in waitingRecipeSO.kitchenObjectSOList)
+                {
+
+                    bool ingredientFound = false;
+                    foreach (KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.getKitchenObjectSOList())
+                    {
+                        ingredientFound = true;
+                        break;
+                    }
+
+                    if (!ingredientFound)
+                    {
+                        return false;
+                    }
+                }
+
+                
+                Debug.Log("Player delivered the correct recipe!"); 
+                waitingRecipeSOList.RemoveAt(i);
+                return true;
+                
+            }
+            
+            
+        }
+
+        return false;
+    }
 }
